fa2d0add329b5d5a5b952b8047816d365db40875
Shahi-ksd <86661301+Pinky-Mwol@users.noreply.github.com>
2021-06-30 17:28:38 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -9,7 +9,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
 const Language = require('../language');
-const Lang = Language.getString('tagall');
+const Lang = Language.getString('tag');
 
 async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
@@ -19,7 +19,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
+Asena.addCommand({pattern: 'tag ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
 
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
